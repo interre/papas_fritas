1989d60d6cf4f2a43250dc338aa36115c87ba95c
interre <lunaalan612@gmail.com>
2023-06-29 08:35:55 -0600

    proyecto inicializado

---
+++ src/main/java/com/mycompany/papa_asada/Papa_asada.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.papa_asada;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Papa_asada {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
